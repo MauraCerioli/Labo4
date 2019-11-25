705b7c60446ba46d2b97d24f0e464177f7f01ec0
Maura Cerioli <maura.cerioli@unige.it>
2019-11-25 15:21:24 +0100

    Implement add test for basic cases add parametric test for OK cases

--- MacroExpansion/MacroExpansionClass.cs
+++ MacroExpansion/MacroExpansionClass.cs
@@ -4,9 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MacroExpansion
+namespace MacroExpansionNS
 {
     public class MacroExpansionClass
     {
+        public static IEnumerable<T> MacroExpansion<T>(IEnumerable<T> sequence, IEnumerable<T> newValues, T value){
+            if (sequence == null)
+                throw new ArgumentNullException(nameof(sequence));
+            if (newValues == null)
+                throw new ArgumentNullException(nameof(newValues));
+            return UnsafeMacroExpansion(sequence,newValues,value);
+        }
+
+        private static IEnumerable<T> UnsafeMacroExpansion<T>(IEnumerable<T> sequence, IEnumerable<T> newValues, T value){
+            foreach (var e in sequence)
+                if (Equals(e, value))
+                    foreach (var v in newValues)
+                        yield return v;
+                else
+                    yield return e;
+        }
     }
 }
--- MacroExpansionTests/MacroExpansionTestClass.cs
+++ MacroExpansionTests/MacroExpansionTestClass.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using MacroExpansionNS;
+using static MacroExpansionNS.MacroExpansionClass;
 
-namespace MacroExpansionTests
-{
-    public class MacroExpansionTestClass
-    {
+namespace MacroExpansionTests{
+    [TestFixture]
+    public class MacroExpansionTestClass{
+        [Test]
+        public void NullSequenceThrowsANE(){
+            Assert.That(() => MacroExpansion(null, new[]{1, 2}, 6).ToArray(),
+                Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void NullNewValuesThrowsANE(){
+            Assert.That(() => MacroExpansion(new[]{1, 2}, null, 6),
+                Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void ReplaceHeadOnceOk(){
+            var result = MacroExpansion(new[]{"pippo", "pluto"}, new[]{"paperino"}, "pippo");
+            var expected = new[]{"paperino", "pluto"};
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ReplaceTailOnceOk(){
+            var result = MacroExpansion(new[]{"pippo", "pluto"}, new[]{"paperino"}, "pluto");
+            var expected = new[]{"pippo", "paperino"};
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ReplaceMiddleOnceOk(){
+            var result = MacroExpansion(new[]{"pippo", "pluto", "minnie"}, new[]{"paperino"}, "pluto");
+            var expected = new[]{"pippo", "paperino", "minnie"};
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        [TestCase(new[]{"pippo", "pluto", "minnie"},new[]{"paperino"},"pluto",new[]{"pippo", "paperino", "minnie"})]
+        [TestCase(new[]{"pippo", "pluto", "minnie"},new[]{"paperino", "qui", "quo"},"pluto",new[]{"pippo", "paperino", "qui", "quo", "minnie"})]
+        [TestCase(new[]{"pippo", "pluto", "minnie"},new string[]{},"pluto",new[]{"pippo", "minnie"})]
+        [TestCase(new[]{"pippo", "pluto", "minnie"},new[]{"paperino"},"paperinik",new[]{"pippo", "pluto", "minnie"})]
+        public void TestOk(string[] sequence, string[] newValues, string value, string[]expected){
+            var result = MacroExpansion(sequence, newValues, value);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
     }
 }
